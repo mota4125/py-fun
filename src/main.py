c2d62be32f1209921ce376c46a193e8fffe34780
mohdtariq <mota4125@bht-berlin.de>
2025-11-22 19:01:35 +0100

    rebase b1-expressions'

--- src/main.py
+++ src/main.py
@@ -1,6 +1,9 @@
-from .calculator import main as main_calculator
+# from .calculator import main as main_calculator
 from .expressions import main as main_expressions
 
 def main():
     # main_calculator()
     main_expressions()
+
+# def main():
+#     main_calculator()
